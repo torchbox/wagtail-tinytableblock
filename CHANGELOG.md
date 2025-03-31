ced9960fab81a66033a0c676d544b202abc04a96
zerolab <dan@zerolab.org>
2025-03-31 10:43:51 +0100

    Add cell/row props buttons to the toolbar

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [unreleased]
 
+## [0.2.3] - 2025-03-31
+
+### Changed
+
+- Added cell/row props buttons to the toolbar
+
 ## [0.2.2] - 2025-03-19
 
 ### Changed
--- src/wagtail_tinytableblock/static/wagtail_tinytableblock/js/tiny-table-block.js
+++ src/wagtail_tinytableblock/static/wagtail_tinytableblock/js/tiny-table-block.js
@@ -4,8 +4,8 @@ class TinyTableBlockDefinition extends window.wagtailStreamField.blocks.FieldBlo
     const block = super.render(placeholder, prefix, initialState, initialError);
 
     let plugins = "table autoresize";
-    let toolbar = "undo redo copy paste | tablerowheader tablecolheader tablemergecells tablesplitcells | tableinsertcolbefore tableinsertcolafter tableinsertrowbefore tableinsertrowafter tabledelete";
-    let contextmenu = "copy paste table";
+    let toolbar = "undo redo | tablerowheader tablecolheader tablemergecells tablesplitcells tablecellprops tablerowprops| tableinsertcolbefore tableinsertcolafter tableinsertrowbefore tableinsertrowafter tabledelete";
+    let contextmenu = "table";
     let valid_elements = "br,table[border|width|height|align|summary],tr[align|valign],td[align|valign|width|colspan|rowspan],th[align|valign|width|colspan|rowspan|scope],thead,tbody";
     if (this.meta.enableLinks) {
         plugins += " link autolink";
